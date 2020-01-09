481e6bc46fed81ad044cf52985429ec6d9a61dd2
Heena Shaik <shaikheena579@gmail.com>
2020-01-09 14:49:49 +0530

    contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,6 @@ export default () => (
         <div class="container shadow m-3 p-3 ">
             <h1 className="display-4 text-warning">Contact</h1>
             <p className="pl-3 text-primary">heena@heena.me</p>
-            <p className="pl-3"> <strong>Address</strong>: <span>#20-13-18,Sanagdigunta Golusula Bhavi Street,Guntur 522003</span></p>
             <p className="pl-3"> <strong>Phone</strong>: <span >9490955950</span></p>
             <p className="pl-3"> <strong>Email</strong>:<span>shaikheena579@gmail.com</span></p>
         </div>
